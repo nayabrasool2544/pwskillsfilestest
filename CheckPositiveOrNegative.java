c9cde9584b738b93213ef021d1d0b185e5994124
nayabrasool2544 <111410746+nayabrasool2544@users.noreply.github.com>
2023-07-04 15:20:00 +0530

    Add files via upload
    This is program for positive or negative number

---
+++ CheckPositiveOrNegative.java
@@ -0,0 +1,20 @@
+public class CheckPositiveOrNegative  
+{  
+public static void main(String[] args)   
+{  
+  
+int num=912;  
+ 
+if(num>0)  
+{  
+System.out.println("The number is positive.");  
+}  
+ 
+
+  
+else  
+{  
+System.out.println("The number is Negative.");  
+}  
+}  
+}  
