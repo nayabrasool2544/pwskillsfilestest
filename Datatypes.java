9b824775b134634ce33d2e0d26b8041ef8ef116c
nayabrasool2544 <111410746+nayabrasool2544@users.noreply.github.com>
2023-07-04 15:28:45 +0530

    Add files via upload
    This is a java program to declare and initialize diff datatypes

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes
+{
+	public static void main(String[] args) {
+		int number = 10;
+		double bignum = 20.30;
+		String name = "SYED";
+		char ch= 'R';
+		boolean value = false;
+		System.out.println( "int datatype is " + number);
+		System.out.println( "double datatype is  " + bignum);
+		System.out.println("String datatype is " + name);
+		System.out.println("char datatype is " + ch);
+		System.out.println( "boolean datatype type is " + value);
+	}
+}
